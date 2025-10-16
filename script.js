5518e3cf7270818c067a2e1e7b85611534127b4f
osissmk87654-crypto <osissmk87654@gmail.com>
2025-10-16 10:13:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// 1. Pilih tombol berdasarkan ID-nya
+const tombolsapa=documen.getElemenById('sapaButton');
+// 2. Tambahkan 'even listener' untuk aksi 'click'
+tobolSapa.addEventlistener('click', function() {
+// 3. jalan kan kode ini ketika tombol di-klik
+    alert('halo! Terimakasih sudah berkunjung!')
